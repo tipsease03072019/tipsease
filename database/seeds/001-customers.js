cd8b281191758df408c1f266e9edd85f0c54314f
KROHNICK <business.kanekperez@gmail.com>
2019-03-11 11:36:43 -0700

    knex init, migrations and seed for customers

---
+++ database/seeds/001-customers.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("customers")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("customers").insert([
+        {
+          id: 1,
+          username: "kanekperez",
+          password: "pass",
+          email: "kanekperez@email.com",
+          img_url: "https://www.fillmurray.com/640/360"
+        },
+        {
+          id: 2,
+          username: "apple",
+          password: "pie",
+          email: "applepie@email.com",
+          img_url: "https://loremflickr.com/640/360"
+        },
+        {
+          id: 3,
+          username: "empty",
+          password: "user",
+          email: "emptyuser@email.com",
+          img_url: "http://via.placeholder.com/640x360"
+        }
+      ]);
+    });
+};
