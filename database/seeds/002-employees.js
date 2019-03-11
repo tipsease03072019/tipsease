605621c18c7c2f489374d6091e7e061e188aed56
KROHNICK <business.kanekperez@gmail.com>
2019-03-11 11:40:42 -0700

    migration and seed for employees

---
+++ database/seeds/002-employees.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("employees")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("employees").insert([
+        {
+          id: 1,
+          username: "employee",
+          password: "pass",
+          email: "employee@email.com",
+          img_url: "https://www.fillmurray.com/640/360"
+        },
+        {
+          id: 2,
+          username: "cherry",
+          password: "pie",
+          email: "cherrypie@email.com",
+          img_url: "https://loremflickr.com/640/360"
+        },
+        {
+          id: 3,
+          username: "full",
+          password: "user",
+          email: "fulluser@email.com",
+          img_url: "http://via.placeholder.com/640x360"
+        }
+      ]);
+    });
+};
