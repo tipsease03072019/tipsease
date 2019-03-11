b5735ddccfe0a2c3612d1307e296c3713a9d6364
KROHNICK <business.kanekperez@gmail.com>
2019-03-11 10:27:10 -0700

    back to root folder

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+// const configureRoutes = require('../config/routes.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// configureRoutes(server);
+
+server.get("/", (req, res) => {
+  res.send("Tipsease");
+});
+
+module.exports = server;
