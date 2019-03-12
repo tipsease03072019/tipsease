4464bd078f2224b73c992c9fb7cd1a69461282a3
unknown <lappjeff20@gmail.com>
2019-03-12 15:30:04 -0400

    Nav overly now toggles properly anywhere on the page

--- dist/components/nav.js
+++ dist/components/nav.js
@@ -2,7 +2,7 @@ const icon = document.querySelector('.hamburger');
 const navDisplay = document.querySelector('.nav-display');
 
 let toggleClass = function () {
-    icon.classList.toggle('test')
+    navDisplay.classList.toggle('display');
 }
 
 icon.addEventListener('click', toggleClass)
