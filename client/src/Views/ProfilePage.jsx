16105230d1d80ae59f8d5d2841096a37873e45a2
Maksim V <maks112v@gmail.com>
2019-03-12 15:02:14 -0400

    Update ProfilePage.jsx

--- client/src/Views/ProfilePage.jsx
+++ client/src/Views/ProfilePage.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, {Component} from "react";
 
-const ProfilePage = () => {
-  state = {};
-  return (
-    <>
-      <h2>Welcome *NAME*</h2>
-      <h4>User Details</h4>
-      {/* USER DETAILS. To Be Made into forms when 'edit user details is clicked' */}
-      <p>Name</p>
-      <p>Age</p>
-      <p>Email</p>
-      <p>Phone</p>
-      <button onClick={() => clickHandler()}>Edit User Details</button>
-    </>
-  );
-};
+class ProfilePage extends Component {
+  state = {
+    test: null,
+  };
+  render() {
+    return (
+      <>
+        <h2>Welcome *NAME*</h2>
+        <h4>User Details</h4>
+        {/* USER DETAILS. To Be Made into forms when 'edit user details is clicked' */}
+        <p>Name</p>
+        <p>Age</p>
+        <p>Email</p>
+        <p>Phone</p>
+        <button >Edit User Details</button>
+      </>
+    );
+  }
+}
 
 export default ProfilePage;
