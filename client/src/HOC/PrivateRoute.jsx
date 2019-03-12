1a5bab72d8abdef0cfd7c9bfb17ca8d82539d934
Maksim V <maks112v@gmail.com>
2019-03-12 15:01:06 -0400

    more styles

--- client/src/HOC/PrivateRoute.jsx
+++ client/src/HOC/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import {Route, Redirect} from "react-router-dom";
 
-const PrivateRouter = ({ component: Component, ...rest }) => {
+const PrivateRouter = ({component: Component, ...rest}) => {
   return (
     <Route
       {...rest}
