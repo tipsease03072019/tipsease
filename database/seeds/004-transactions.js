1b241524aff6cfd0f07fe79f1bf5dfb78728f9eb
KROHNICK <business.kanekperez@gmail.com>
2019-03-14 13:47:22 -0700

    changed uid in trans tbl

--- database/seeds/004-transactions.js
+++ database/seeds/004-transactions.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("transactions").insert([
-        { id: 1, users_id: 1, tip: 5 },
-        { id: 2, users_id: 1, tip: 10 },
-        { id: 3, users_id: 3, tip: 15 }
+        { id: 1, uid: "f467StrA0ZZ8MAPoe2zcDQk53hO1", tip: 5 },
+        { id: 2, uid: "f467StrA0ZZ8MAPoe2zcDQk53hO2", tip: 10 },
+        { id: 3, uid: "f467StrA0ZZ8MAPoe2zcDQk53hO3", tip: 15 }
       ]);
     });
 };
