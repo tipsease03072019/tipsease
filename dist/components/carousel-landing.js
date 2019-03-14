0e5606246d5f13005e83efffe7ca9a22f91d9f40
unknown <lappjeff20@gmail.com>
2019-03-14 15:50:28 -0400

    Fixed slides not sliding

--- dist/components/carousel-landing.js
+++ dist/components/carousel-landing.js
@@ -8,10 +8,19 @@ const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 //arrange the slides next to each other
-// slides[0].style.left = slideWidth * 0 + 'px';
-// slides[1].style.left = slideWidth * 1 + 'px';
-// slides[2].style.left = slideWidth * 2 + 'px';
 
-slides.forEach(function(slide, index) {
+const setSlidePosition = function(slide, index) {
     slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition)
+
+//click the right button, move to the right
+nextButton.addEventListener('click', function(e) {
+    const currentSlide =  track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const amountToMove = nextSlide.style.left;
+    //move to the next slide
+    console.log(currentSlide)
+    track.style.transform = `translateX(-${amountToMove})`;
+
 })
