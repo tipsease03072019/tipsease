8e508463c5fb5289751426ec8b1c394948abea8c
unknown <lappjeff20@gmail.com>
2019-03-14 18:10:59 -0400

    finished carousel along with carousel styling

--- dist/components/carousel-landing.js
+++ dist/components/carousel-landing.js
@@ -14,14 +14,65 @@ const setSlidePosition = function(slide, index) {
 };
 slides.forEach(setSlidePosition)
 
-//click the right button, move to the right
+const moveToSlide = function (track, currentSlide, targetSlide) {
+    track.style.transform = `translateX(-${targetSlide.style.left})`;
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const hideNavIcons = function(slides, prevButton, nextButton, targetIndex) {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden')
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        console.log('test')
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+//click the right button, move slide to the right
 nextButton.addEventListener('click', function(e) {
     const currentSlide =  track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
     const amountToMove = nextSlide.style.left;
+    const nextIndex = slides.findIndex(function(slide) {
+        return slide === nextSlide;
+    })
     //move to the next slide
-    track.style.transform = `translateX(-${amountToMove})`;
-    currentSlide.classList.remove('current-slide');
-    nextSlide.classList.add('current-slide');
+    moveToSlide(track, currentSlide, nextSlide)
+    hideNavIcons(slides, prevButton, nextButton, nextIndex)
+
+})
+
+//click the left button, move slide to left
+prevButton.addEventListener('click', function(e) {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const prevIndex = slides.findIndex(function(slide) {
+        return slide === prevSlide;
+    })
+
+    hideNavIcons(slides, prevButton, nextButton, prevIndex)
+    moveToSlide(track, currentSlide, prevSlide)
+})
+
+dotsNav.addEventListener('click', function(e) {
+    const targetDot = e.target.closest('a');
+    console.log(targetDot)
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(function(dot) {
+        return dot === targetDot;
+    })
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide)
+    hideNavIcons(slides, prevButton, nextButton, targetIndex)
 
 })
