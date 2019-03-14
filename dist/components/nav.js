4dca948edee285e556d2b87ebd572a1dada5c52a
unknown <lappjeff20@gmail.com>
2019-03-14 12:06:41 -0400

    More work on hamburger nav

--- dist/components/nav.js
+++ dist/components/nav.js
@@ -2,29 +2,25 @@ const icon = document.querySelector('.hamburger');
 const iconLines = document.querySelectorAll('.line')
 const navDisplay = document.querySelector('.nav-display');
 const desktop = window.matchMedia( '(min-width: 1000px)' );
-
-console.log(iconLines)
+const mobile = window.matchMedia( '(max-width: 800px)' );
 
 
 let animateMenu = new TimelineMax({paused: true, reversed: true})
 let animateMenuDesktop = new TimelineMax({paused: true, reversed: true})
 let animateLines = new TimelineMax({paused: true, reversed: true})
 
-//watches desktop width
+//watches desktop and tablet width
+mobile.addListener;
 desktop.addListener;
 
 //toggle animations on hamburger icon to morph into an X
 let toggleMenu = function() {
 
-    if(!desktop.matches) {
-        animateLines.to(icon, .3, {rotation: 180})
-        .to(iconLines, .1, {background: '#67DA8C'})
-        .to(iconLines[1], .3, {width: 0})
-        .to(iconLines[0], .1, { rotation: 45, x: '7px', y: '12px'})
-        .to(iconLines[2], .1, { rotation: -45, x: '7px', y: '-8px'})
-
-        animateLines.reversed() ? animateLines.play() : animateLines.reverse();
-    } else {
+        if (mobile.matches) {
+            iconLines.forEach(function(line) {
+                line.classList.toggle('color-change')
+            })
+        }
 
         animateLines.to(icon, .3, {rotation: 180})
         .to(iconLines[1], .3, {width: 0})
@@ -32,9 +28,8 @@ let toggleMenu = function() {
         .to(iconLines[2], .1, { rotation: -45, x: '7px', y: '-8px'})
 
         animateLines.reversed() ? animateLines.play() : animateLines.reverse();
-    }
-
 }
+
 
 //toggles nav dropdown
 let toggleDisplay = function () {
